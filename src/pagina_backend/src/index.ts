6cf63fe03c86ae1a1cc452388642bd41d66d37fc
Montserrat  Resendiz <utm23090618@utma.edu.mx>
2024-07-12 17:24:12 -0600

    first commit

---
+++ src/pagina_backend/src/index.ts
@@ -0,0 +1,9 @@
+import { Server } from 'azle';
+import express from 'express';
+import cors from 'cors'
+
+export default Server(() =>{
+    const app = express();
+    app.use(cors());
+    return app.listen(); 
+})
