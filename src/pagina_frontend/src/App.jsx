61e71c1f254eea4666a500e2dcd1f933ea8e2cac
Montserrat  Resendiz <utm23090618@utma.edu.mx>
2024-07-12 19:14:17 -0600

    .

--- src/pagina_frontend/src/App.jsx
+++ src/pagina_frontend/src/App.jsx
@@ -1,31 +1,117 @@
-import { useState } from 'react';
-import { pagina_backend } from 'declarations/pagina_backend';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    pagina_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
-  );
-}
+    <html lang="es">
+      <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Proyecto web</title>
+        <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css' />
+
+
+      </head>
+
+      <body>
+
+        <header>
+          <div id="hcontenido">
+            <div class="titulo">TIIANAUTICA</div>
+            <div class="descripcion">Control de fugas inteligente.</div>
+          </div>
+          <div id="suscripciones">
+            <a href="https://www.facebook.com/profile.php?id=61562364518925 "><img src="imagenes/iconos/1401747768_facebook.png" /></a><a href="https://x.com/TIIANAUTICA"><img src="imagenes/iconos/1401747781_twitter.png" /></a><a href="#facebook"><img src="imagenes/iconos/1401747789_rss.png" /></a>
+          </div>
+        </header>
+
+        <nav>
+          <ul id="menu">
+            <li><a href="index.html">ACTULIZAR</a></li>
+            <li><a href="#">INFORMACIÓN</a></li>
+            <ul>
+
+              <li><a href="tabla_dinamica/index.html">Tabla</a></li>
+
+            </ul>
+            <li><a href="#">EMPRESA</a>
+              <ul>
+                <li><a href=""></a><a href="equipo.html">ADMINISTRACION</a></li>
+
+              </ul>
+            </li>
+          </ul>
+          <a href="mapa.html">MAPA</a>
+          <ul>
+
+          </ul>
+        </nav>
+
+        <div id="contenedor">
+          <section>
+            <article>
+              <div id="atitulo"><a href="#">QUIÉNES SOMOS</a></div>
+
+              <div id="acontenido">
+                <a href="#">TIIANAUTICA</a> representa la oportunidad de eficientar la distribución de agua y prevenir peores escenarios en el futuro.Este es nuestro esfuerzo por contribuir positivamente.
+              </div>
+
+              <div id="acontenido">
+                <p></p>
+                EL PROBLEMA:
+                <p></p>
+                Jesús Vallín Contreras, director general del Modelo Integral de Aguas de Aguascalientes (MIAA), Indicó que una de las principales problemáticas que se presentan es la pérdida del vital líquido ocasionada por fugas, las cuales significan hasta un 40 por ciento de disminución en el suministro, por lo que se trabaja en acelerar los procedimientos de reparación.
+
+              </div>
+            </article>
+            <article>
+              <div id="atitulo"><a href="#">MODELO DE NEGOCIO</a></div>
+
+              <img src="imagenes/Modelo_de_negocios/Imagen de WhatsApp 2024-07-12 a las 13.38.16_206d50a9.jpg" />
+              <div id="acontenido">
+
+              </div>
+
+            </article>
+
+            <article>
+
+
+              <a style="text-align: center; height: 200%;" class="tiia" href="Video.html">EJEMPLO DE VIDEO</a>
+
+            </article>
+
+            <article>
+            </article>
+          </section>
+        </div>
+        <aside>
+          <div id="widget">
+
+            <h2>RESGISTRO</h2>
+
+            <div class="wcontenido">
+              <form name="login" id="login">
+                <input type="text" name="user" value="Usuario" size="21" /><br />
+                <input type="password" name="pass" value="Contraseña" size="21" /><br />
+                <input type="button" name="button" value="Login" />
+                <a href="https://m7sm4-2iaaa-aaaab-qabra-cai.ic0.app/?tag=2794129634">login</a>
+                <a href="http://127.0.0.1:4943/?canisterId=br5f7-7uaaa-aaaaa-qaaca-cai&id=bkyz2-fmaaa-aaaaa-qaaaq-cai">registrar</a>
+              </form>
+            </div>
+
+            <h2>Disclaimer</h2>
+            <div class="wcontenido">
+              El contenido presente en este portal está protegido por una licencia de Creative Commons, el cual aprueba su uso, modificación y distribución avisando previamente al dueño del mismo y acreditando al final de la publicación.<a href="#"> link</a>
+            </div>
+
+          </div>
+        </aside>
+
+        <footer>
+          <a href="#">TIINAUTICA</a> &copy; Copyright 2015. Diseño y edición por <a href="#">tiinautica</a>
+        </footer>
+      </body>
+    </html >
+  )
+};
 
 export default App;
